eb507fc72d6ce25dfd73ffa8a4596f271b592d7d
mayu37 <140620453+mayu37@users.noreply.github.com>
2023-07-29 19:04:40 +0530

    Update Program19.java

--- NestedforLoop/Program19.java
+++ NestedforLoop/Program19.java
@@ -9,24 +9,25 @@ package NestedforLoop;
 public class Program19 {
 	public static void main(String[] args) {
 		int num =4;
-		int a=1;
 		for(int i=1;i<=num;i++) {
-			int temp =a;
+			int temp = i;
 			for(int j=1;j<=i;j++) {
 				if(i%2!=0) {
-					System.out.print(temp*temp*temp+"\t");
-				}else if(j%2==0) {
-					System.out.print(temp*temp+"\t");
-				}else if(j%2!=0) {
-					System.out.print(temp*temp*temp+"\t");
-				}else {
-					System.out.print(temp*temp+"\t");
+					if(j%2==0) {
+						System.out.print(temp*temp*temp+"\t");
+					}else {
+						System.out.print(temp*temp+"\t");
+					}
+					}else {
+						if(j%2!=0) {
+							System.out.print(temp*temp*temp+"\t");
+						}else {
+							System.out.print(temp*temp+"\t");
+						}
+					}
+				temp++;
 				}
+				System.out.println();
 			}
-			temp++;
 		}
-		a++;
-		System.out.println();
-	}
-
 }
